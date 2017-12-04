513ac12c251920591664feab37db35362f26befe
Kamil Serafin <kserafin@gmail.com>
2017-12-04 20:30:22 +0100

    GCM shows hierarhy of company

--- src/main/java/task1/CompanyGeneratorMain.java
+++ src/main/java/task1/CompanyGeneratorMain.java
@@ -1,21 +1,29 @@
 package task1;
 
 
-import task1.Service.IntroducingService;
-
 public class CompanyGeneratorMain {
 
     public static void main(String[] args) {
-
 
         CompanyGenerator company = new CompanyGenerator();
 
         company.generateCompany(3);
 
-        System.out.println(company.getManagers());
+        int k = 2;
+        int n = 0;
 
-        for(int i =0; i < company.numOfCeo; i++) {
-            System.out.println(IntroducingService.introduceCompany(company.getManagers().get(i)));
+        for(int i = 0; i < company.numOfCeo; i++) {
+            System.out.println("CEO");
+            System.out.println(company.getManagers().get(i));
+            System.out.println("TEAM_MANAGERS");
+            System.out.println(company.getManagers().get(i).getEmployees());
+            for(int j = company.numOfCeo+n; j < company.numOfCeo+k; j++) {
+                System.out.println("DEVELOPERS");
+                System.out.println(company.getManagers().get(j).getEmployees());
+            }
+            System.out.println("\n\n");
+            k = k + 2;
+            n = n + 2;
         }
 
     }
